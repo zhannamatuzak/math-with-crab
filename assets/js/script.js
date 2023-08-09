3b8fdca8cd66f45a31bd6a6c726976ecb9a0e236
zhannamatuzak <volokhovazhanna@gmail.com>
2023-08-09 10:26:40 +0200

    Add style to pink button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-$('#logo-image').animate({
-  height: 'toggle'
-},5000);
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("pink-btn");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
