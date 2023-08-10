0cad9ce6f83431c53adfa97b2bb01ab878c762f5
zhannamatuzak <volokhovazhanna@gmail.com>
2023-08-10 12:59:45 +0200

    Add event listener for function runMainScreen and code to this function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 /**
  * Wait for the DOM to filish loading befor running the game
- * Run the main screen with greeting and user (child) login
+ * Run the main screen with greeting and user (child) log-in
 */
-
 document.addEventListener('DOMContentLoaded', function () {
 runMainScreen();
 });
@@ -10,17 +9,30 @@ runMainScreen();
 /**
  * Set up of game variables to vary display/hide
  */
-
-let greeting = document.getElementById("get-name");
-let childName = document.getElementById('child-name');
+let greeting = document.getElementById("user-input");
+let childName = document.getElementById("child-name");
 let errorMessage = document.getElementById("error-message");
-let gameArea = document.getElementById('game-area');
-let rules = document.getElementById('rules');
-let gameSet = document.getElementById('game-set');
-let startBtn = document.getElementById('start-btn');
-let newGame = document.getElementById('new-game');
+let rules = document.getElementById("rules-btn");
+let gameSet = document.getElementById("game-set");
+let startBtn = document.getElementById("start-btn");
+let newGame = document.getElementById("new-game");
+let scoreArea = document.getElementById("score-area");
+let timeDisplay = document.getElementById("time-display");
 
+/**
+* Show the main screen with greeting and user (child) log-in
+*/
 function runMainScreen() {
+  greeting.style.display = "block";
+  errorMessage.style.display = "none";
+  rules.style.display = "none";
+  gameSet.style.display = "none";
+  startBtn.style.display = "none";
+  newGame.style.display = "none";
+  scoreArea.style.display = "none";
+  childName.style.display = "none";
+  document.getElementById("child-name").innerText = "";
+  document.getElementById("user").focus();
 }
 
 function checkUsername() {
