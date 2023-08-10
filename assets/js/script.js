06c4148729f244a6c61bd35dceadd4989c5f8a60
zhannamatuzak <volokhovazhanna@gmail.com>
2023-08-10 10:25:17 +0200

    Set up of game variables to vary display and hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+/**
+ * Wait for the DOM to filish loading befor running the game
+ * Run the main screen with greeting and user (child) login
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+runMainScreen();
+});
+
+/**
+ * Set up of game variables to vary display/hide
+ */
+
+let greeting = document.getElementById("get-name");
+let childName = document.getElementById('child-name');
+let errorMessage = document.getElementById("error-message");
+let gameArea = document.getElementById('game-area');
+let rules = document.getElementById('rules');
+let gameSet = document.getElementById('game-set');
+let startBtn = document.getElementById('start-btn');
+let newGame = document.getElementById('new-game');
+
 function runMainScreen() {
 }
 
